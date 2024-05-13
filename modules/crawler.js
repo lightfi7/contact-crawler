05f6b41401e7c14746cf4130d86dc7a510e29aac
Fi <158217226+lightfi7@users.noreply.github.com>
2024-05-13 14:12:54 -0700

    Update crawler.js

--- modules/crawler.js
+++ modules/crawler.js
@@ -3,25 +3,24 @@ const cheerio = require("cheerio");
 const extractDataFromHtml = (html) => {
   try {
     const $ = cheerio.load(html, { decodeEntities: false });
-    const emailPattern = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}/g;
+    const emailPattern = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
     const phonePattern =
-      /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/g;
+      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/g;
     const socialPattern =
-      /https?:\/\/(www\.)?(facebook|twitter|instagram|youtube\.com|linkedin\.com)\.com\/\w+/g;
-
+      /(?:https?:\/\/)?(?:www\.)?(?:facebook|fb|twitter|linkedin|instagram|youtube)\.com\/(?:[\w\-\.]+\/?)+/g;
     const extractedData = {
-      emails: extractMatches(html, emailPattern),
-      phoneNumbers: extractMatches(html, phonePattern),
-      socialLinks: extractSocialLinks($, socialPattern),
+      email_addresses: extractMatches(html, emailPattern),
+      phone_numbers: extractMatches(html, phonePattern),
+      social_links: extractSocialLinks($, socialPattern),
     };
 
     return extractedData;
   } catch (error) {
     console.error("Error extracting data from HTML:", error);
     return {
-      emails: [],
-      phoneNumbers: [],
-      socialLinks: [],
+      email_addresses: [],
+      phone_numbers: [],
+      social_links: [],
     };
   }
 };
